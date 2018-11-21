b016fc8c7c8384f54ecfd252a5d4ee2420817648
wowowo12 <finlinfin@gmail.com>
2018-11-21 11:06:55 +0900

    181121

--- public/js_exam/js/sound.js
+++ public/js_exam/js/sound.js
@@ -2,7 +2,7 @@
 // $("#snd") ->jQuery
 // document.getElementById("snd") ES5
 // document.querySelector("#snd") ES6
-                                                                                                                                              
+
 
 
 // console.log($("#snd")[0]) jQuery를 javascript 데이터로 가져올때
@@ -43,27 +43,49 @@ $("#bt_play").click(function(){
 });
 */
 // 3번 순수 자바스크립트
+
 var snd = document.querySelector("#snd");
 var btPlay = document.querySelector("#bt_play");
 btPlay.addEventListener("click", soundPlay);
 
-function soundPlay(){
-	if(snd.paused) {
+function soundPlay() {
+	if (snd.paused) {
 		snd.play();
 		this.classList.toggle("fa-play-circle", false);
 		this.classList.toggle("fa-pause-circle", true);
-	}
-	else {
+	} else {
 		snd.pause();
 		this.classList.toggle("fa-play-circle", true);
 		this.classList.toggle("fa-pause-circle", false);
 	}
 }
-$("#sel_snd").on("change",function(){
+$("#sel_snd").on("change", function () {
 	$("#snd")[0].src = $(this).val(); //audio자체의 src속성을 바꿔주는 값을 써야한다. method가 아닌 property(
 	$("#snd")[0].play();
 	$("#bt_play").removeClass("fa-play-circle").addClass("fa-pause-circle");
 })
 
 
-//--->정리 
+$("[data-slider]").on("slider:ready", function (e, data) {
+	$("#snd")[0].volume = 0.5;
+});
+$("[data-slider]").on("slider:changed", function (e, data) {
+	$("#snd")[0].volume = data.value.toFixed(1);
+});
+
+$("#bt_volume").on("click", function () {
+	if ($(this).hasClass("fa-volume-up")) 
+	{
+		$(this).removeClass("fa-volume-up").addClass("fa-volume-off");
+		$("#snd")[0].muted =true;
+	
+	}
+	else($(this).hasClass("fa-volume-off"))
+	{
+		$(this).removeClass("fa-volume-off").addClass("fa-volume-up");
+		$("#snd")[0].muted = false;
+	
+	}
+})
+
+//--->정리
